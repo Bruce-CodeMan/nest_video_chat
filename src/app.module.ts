c5468176840cdc1f2d7dcb383187c14b31e3662c
Bruce Hsu <1033684650@qq.com>
2024-04-09 14:52:19 +0800

    create a orm.config.ts file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,29 +1,25 @@
 // Import the core libraries
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from "@nestjs/typeorm";
+import { ConfigModule } from "@nestjs/config";
 
 // Import the libraries from the external
 import { config } from "dotenv";
 
 // Import the custom entities
 import { UserModule } from "@/modules/user/user.module";
+import { connectionOptions } from '@/utils/orm.connection';
 
-config();
+const envFilePath = `.env.${process.env.NODE_ENV || `development`}`
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.MYSQL_HOST,
-      port: parseInt(process.env.MYSQL_PORT),
-      username: process.env.MYSQL_USERNAME,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DATABASE,
-      entities: [`${__dirname}/../modules/**/*.entity.{ts,js}`],
-      logging: true,
-      synchronize: true,
-      autoLoadEntities: true
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath,
+      load: [() => config({path: ".env"})]
     }),
+    TypeOrmModule.forRoot(connectionOptions),
     UserModule
   ],
   controllers: [],
--- src/utils/orm.connection.ts
+++ src/utils/orm.connection.ts
@@ -1,1 +1,56 @@
+// Import the core libraries
 import * as fs from "fs";
+import * as path from "path";
+
+// Import the libraries from the external
+import * as dotenv from "dotenv"
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { DbEnum } from "@/enum/config.enum";
+import { DataSource, DataSourceOptions } from "typeorm";
+
+// Import the custom files
+
+
+// Read the environment path
+function getEnv(envFilePath: string): Record<string, any> {
+  const envPath = path.resolve(process.cwd(), envFilePath)
+  if(fs.existsSync(envPath)) {
+    return dotenv.parse(fs.readFileSync(envPath))
+  }
+  return {}
+}
+
+// Parse the boolean value
+function getBooleanValue(value: string): boolean {
+  return value == "true"
+}
+
+// Create the mysql connection
+function buildConnectionOptions() {
+
+  const defaultConfig = getEnv(".env")
+  const envConfig = getEnv(`.env.${process.env.NODE_ENV || `development`}`)
+  const config = {...defaultConfig, ...envConfig}
+
+  const dbPort = config[DbEnum.DB_PORT] ? parseInt(config[DbEnum.DB_PORT], 3306) : undefined;
+
+  return {
+    type: config[DbEnum.DB_TYPE],
+    host: config[DbEnum.DB_HOST],
+    port: dbPort,
+    username: config[DbEnum.DB_USERNAME],
+    password: config[DbEnum.DB_PASSWORD],
+    database: config[DbEnum.DB_DATABASE],
+    entities: [`${__dirname}/../modules/**/*.entity.{ts,js}`],
+    synchronize: getBooleanValue(config[DbEnum.DB_SYNC]),
+    logging: getBooleanValue(config[DbEnum.DB_LOGGING])
+  } as TypeOrmModuleOptions;
+}
+
+export const connectionOptions = buildConnectionOptions();
+
+export default new DataSource({
+  ...connectionOptions,
+  migrations: [`${process.cwd()}/src/migrations/*`],
+  subscribers: []
+} as DataSourceOptions);
